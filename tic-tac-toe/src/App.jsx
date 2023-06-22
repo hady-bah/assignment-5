bb86e119b1560e52babdce333b4c63aa83122127
Hady Bah <hadymbah@gmail.com>
2023-06-22 15:44:21 -0400

    final commit

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,34 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function Square() {
-//   return <button className="square">1</button>;
-// }
-
-
-// export default function Board() {
-//   return (
-//     <>
-//       <div className="board-row">
-//         <Square />
-//         <Square />
-//         <Square />
-//       </div>
-//       <div className="board-row">
-//         <Square />
-//         <Square />
-//         <Square />
-//       </div>
-//       <div className="board-row">
-//         <Square />
-//         <Square />
-//         <Square />
-//       </div>
-//     </>
-//   );
-// }
 import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
